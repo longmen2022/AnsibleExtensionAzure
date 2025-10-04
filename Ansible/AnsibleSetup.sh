f0c8c771eef67309090c03a9bbc539f4276072de
men_l <men_long@yahoo.com>
2025-10-04 10:59:01 -0400

    update

--- Ansible/AnsibleSetup.sh
+++ Ansible/AnsibleSetup.sh
@@ -1,7 +1,17 @@
-sudo apt-get update
-sudo apt-get upgrade -y
+#!/bin/bash
+
+# Update and upgrade system packages
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Install pip3
 sudo apt-get install python3-pip -y
+
+# Install Ansible with Azure support
 sudo pip3 install 'ansible[azure]'
+
+# Install Azure Ansible collection
 ansible-galaxy collection install azure.azcollection
-wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+
+# Download and install additional Azure dependencies
+wget https://raw.githubusercontent.com/ansible-collections/azure/main/requirements-azure.txt
 sudo pip3 install -r requirements-azure.txt
